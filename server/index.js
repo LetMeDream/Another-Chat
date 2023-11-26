8ec9c05a4942b75499471e7f3227a7e2f839a207
Raul <raulalfonzo66@gmail.com>
2023-11-25 22:46:38 -0400

    Create the 're-actions' (don't really know the term) for the new custom events emmited in the front ('ping' and 'post_video')

--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer()
-
 const io = require('socket.io')(server, {
-    cors: { origin: '*' }
-})
+    cors: { origin: '*' },
+		maxHttpBufferSize: 1e8
+});
 
 io.on('connection', (socket) => {
-    console.log('Conexión nueva!!!')
+    console.log('Conexión nueva!')
 
     /* 'Reacting' to the 'send_message' custom event emitted on the front */
-    socket.on('send_message', (data) => {
-        // After receiving the data, this is emited to ALL
-        io.emit('add_message', data)
+    socket.on('send_message', (payload) => {
+        // After receiving the payload, this is emited to ALL
+        io.emit('add_message', payload)
     })
+
+    socket.on('ping', (payload) => {
+        console.log(payload)
+        io.emit('notice_me_user', payload)
+    })
+
+		socket.on('post_video', (payload) => {
+			console.log(payload)
+			io.emit('get_video', payload)
+		})
 })
-
-
-
 
 server.listen(3000)
