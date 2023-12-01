8a94e3f418ea7291167da0b3e27f5201c8b72ee7
Raul <raulalfonzo66@gmail.com>
2023-12-01 16:31:06 -0400

    Create peer.ts helper file. It creates a PeerConnection Object that will have the methods reqiured for handling the peer connection

---
+++ front/src/helpers/peer.ts
@@ -0,0 +1,115 @@
+import Peer, {DataConnection} from "peerjs"
+import {message} from "antd" 
+let peer: Peer | undefined
+const connectionMap: Map<string, DataConnection> = new Map<string, DataConnection>()
+
+export enum DataType {
+	FILE = 'FILE',
+	OTHER = 'OTHER'
+}
+
+export interface Data {
+	file?: Blob | MediaSource | undefined
+}
+
+export const PeerConnection = {
+  getPeer: () => peer,
+  startPeerSession: (setIsConnected: (value: boolean) => void, setId: (value: string) => void) => new Promise<string>((resolve, reject) => {
+	try {
+		peer = new Peer()
+		peer.on('open', (id) => {
+			console.log('My ID: ' + id)
+			setId(id)
+			setIsConnected(true)
+			resolve(id)
+		}).on('error', (err) => {
+			console.log(err)
+			setIsConnected(false)
+			message.error(err.message)
+		})
+	} catch (err) {
+		console.log(err)
+		reject()
+	}
+  }),
+  onIncomingConnection: (callback: (conn: DataConnection) => void) => {
+		peer?.on('connection', function (conn) {
+				console.log("Incoming connection: " + conn.peer)
+				connectionMap.set(conn.peer, conn)
+				callback(conn)
+		});
+  },
+	onConectionDisconected: (id: string, callback: () => void) => {
+		if (!peer) {
+			throw new Error("Peer doesn't start yet")
+		}
+		if (!connectionMap.has(id)) {
+				throw new Error("Connection didn't exist")
+		}
+		const conn = connectionMap.get(id)
+		if (conn) {
+			conn.on('close', () => {
+				console.log("Connection closed: " + id)
+				connectionMap.delete(id)
+				callback()
+			})
+		}
+
+	},
+	onConectionReceiveData: (id: string, callback: (data: Data) => void) => {
+		if (!peer) {
+			throw new Error("Peer doesn't start yet")
+		}
+		if (!connectionMap.has(id)) {
+				throw new Error("Connection didn't exist")
+		}
+		const conn = connectionMap.get(id)
+		if(conn){
+			conn.on('data', (receivedData) => {
+				console.log('Receiving data from ' + id)
+				callback(receivedData)
+			})
+		}
+	},
+	connectPeer: (remoteId: string) => new Promise<void>((resolve, reject) => {
+		if (!peer) {
+			reject(new Error("Peer doesn't start yet"))
+			return
+		}
+		if (connectionMap.has(remoteId)) {
+			reject(new Error("Connection already existed"))
+			return
+		}
+		try {
+			const conn = peer.connect(remoteId, {reliable: true})
+			if (!conn) {
+					reject(new Error("Connection can't be established"))
+			} else {
+					conn.on('open', function() {
+							console.log("Connect to: " + remoteId)
+							connectionMap.set(remoteId, conn)
+							resolve()
+					}).on('error', function(err) {
+							console.log(err)
+							reject(err)
+					})
+			}
+	} catch (err) {
+			reject(err)
+	}
+	}),
+	sendData: (remoteId: string, data: Data | string) => new Promise((resolve, reject) => {
+		if (!connectionMap.has(remoteId)) {
+			reject(new Error("Connection doesn't exist"))
+			return
+		}
+		try {
+			const conn = connectionMap.get(remoteId)
+			if (conn) {
+				conn.send(data)
+			}
+		} catch (error) {
+			reject(error)
+		}
+	})
+}
